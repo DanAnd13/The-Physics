df0f34cc3549cf691e88ee460945ab30cc158b5a
unknown <andreyjk13.gmail.com>
2024-03-21 09:51:34 +0200

    first commit

---
+++ Assets/Scripts/SecondsUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeOfStart = 0;
+    bool isStart = false;
+    public float speed = 0.5f;
+    void Update()
+    {
+        if (!isStart)
+        {
+            timeOfStart = Time.realtimeSinceStartup;
+            isStart = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, 
+                                              this.transform.position.y, 
+                                              (Time.realtimeSinceStartup - timeOfStart) * speed);
+    }
+}
